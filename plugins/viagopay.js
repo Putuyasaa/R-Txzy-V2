f5714d6a0fd3b14e147d7055386d2aeea276e122
JunnXD <99121904+Putuyasaa@users.noreply.github.com>
2022-02-12 20:47:37 +0800

    Add files via upload

---
+++ plugins/viagopay.js
@@ -0,0 +1,34 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `╠═〘 PEMBAYARAN 〙 ═
+╠═ Untuk Pembayaran Via PULSA
+╠➥ *087752078682*
+║- Chat Owner Terlebebih
+║ Dahulu
+╠═〘 Fitur 〙 ═
+║
+╠➥ *FITUR WELCOME*
+║    *FITUR STIKER*
+╠➥ *FITUR ADMIN*
+║    *KICK ORANG*
+║    *JADIIN ADMIN*
+╠➥ *FITUR HINDU*
+║   *KITAB SUCI*
+║   *CEK JADWAL TRISANYA*
+╠➥ *INTERNET*
+║   *BRAINLY*
+║   *GOOGLE*
+╠➥ *DAN 200 LEBIH*
+║   *FITUR LAINNYA*
+║ 
+╠═ ©2022 wabot-aq
+╠═ Scrip original by Nurutomo
+╠═〘 JuniArta 〙 ═
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
+}
+handler.help = ['viadana']
+handler.tags = ['info']
+handler.command = /^viadana$/i
+
+module.exports = handler
---
+++ plugins/viapulsa.js
@@ -0,0 +1,33 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `╠═〘 PEMBAYARAN 〙 ═
+╠═ Untuk Pembayaran Via Pulsa +5k
+╠➥ Cht Owner
+║
+╠═〘 Fitur 〙 ═
+║
+╠➥ *FITUR WELCOME*
+║    *FITUR STIKER*
+╠➥ *FITUR ADMIN*
+║    *KICK ORANG*
+║    *JADIIN ADMIN*
+╠➥ *FITUR HINDU*
+║   *KITAB SUCI*
+║   *CEK JADWAL TRISANYA*
+╠➥ *INTERNET*
+║   *BRAINLY*
+║   *GOOGLE*
+╠➥ *DAN 200 LEBIH*
+║   *FITUR LAINNYA*
+║ 
+╠═ ©2022 Rpg wabot-aq
+╠═ Scrip original by Nurutomo
+╠═〘 JuniArta 〙 ═
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
+}
+handler.help = ['viapulsa']
+handler.tags = ['info']
+handler.command = /^viapulsa$/i
+
+module.exports = handler
